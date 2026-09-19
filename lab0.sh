b146fce084e73b310b14e3fe7a9a22d20a414e42
Natalia Dugina <228317627+natalidugina@users.noreply.github.com>
2026-09-19 08:21:14 +0000

    Second part of lab0

--- lab0.sh
+++ lab0.sh
@@ -86,3 +86,12 @@ chmod 640 nagiev_call
 git status
 git add . ../lab0.sh
 git commit -m "A directory and file tree with content has been created, and file and directory permissions have been set"
+git push
+cp nagiev_call claude_monet/owner_office/nagiev_call_copy
+cp -r claude_monet/advertising claude_monet/owner_office/advertising_backup
+ln -s claude_monet/contracts/supplier_contract owner_contract
+ln -s ../hall claude_monet/owner_office/hall_access
+ln claude_monet/contracts/supplier_contract claude_monet/contracts/supplier_duplicate
+cat claude_monet/owner_office/owner_order claude_monet/chef_office/barinov_reply > claude_monet/owner_office/meeting_notes
+cat claude_monet/kitchen/chef_order >> nagiev_call
+mv claude_monet/advertising/promo_plan archive/promo_final
---
+++ lab0/lab0.sh
@@ -0,0 +1,2 @@
+ln -s claude_monet/contracts/supplier_contract owner_contract
+ln -s ../hall claude_monet/owner_office/hall_access
